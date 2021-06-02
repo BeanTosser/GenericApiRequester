45c78571abadeab1f956a6588887f19a5a372900
BeanTosser <56370925+BeanTosser@users.noreply.github.com>
2021-06-02 11:54:30 -0400

    Continue working on endpoints and list pages.

--- src/Components/ContentsList.js
+++ src/Components/ContentsList.js
@@ -1,12 +1,17 @@
 import { Link, Route } from "react-router";
 
-export default function (props) {
-  let toRender = null;
-  if (typeof props.infoObject === "object") {
-    let links = [];
-    let renderableArray = [];
-    for (const [key, value] of Object.entries(props.infoObject)) {
-      links.push(<Link> to={"/" + key}</Link>);
+export default function getContentsListElement(key, value) {
+  if (typeof value !== "object" && typeof value !== "array") {
+    // This is an endpoint. Return a list item with the simple
+    // text of the key and the value
+    return <li>{key + ": " + value}</li>;
+  } else {
+    let renderableList = [];
+    if (Array.isArray(value)) {
+      renderableList = value.map((element) =>
+        getContentsListElement(null, element)
+      );
     }
+    return <div>{renderableList}</div>;
   }
 }
